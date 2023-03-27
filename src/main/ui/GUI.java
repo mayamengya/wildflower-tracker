3cbc00d305ccc08fbdeaa8b5c55475f96881a739
Lemon <maya.dong@ubc.ca>
2023-03-26 23:31:39 -0700

    Began GUI

---
+++ src/main/ui/GUI.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javax.swing.*;
+
+
+// Represents the graphic user interface for Wildflower Tracker
+public class GUI {
+    JFrame frame = new JFrame();
+    JLabel logo = new JLabel();
+
+    // EFFECTS: initializes main menu
+    public GUI() {
+        initializeLogo();
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Wildflower Tracker");
+        frame.setSize(700,500);
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initalizes main menu image
+    public void initializeLogo() {
+        ImageIcon logoImage = new ImageIcon("wildflower-tracker.png");
+        logo.setIcon(logoImage);
+    }
+
+}
---
+++ src/main/ui/UserMain.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class UserMain {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+    }
+}
